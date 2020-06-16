a2b33c4a6d6571a068385783e8c9713d69ad69d9
“dziekan30” <dziekan30@gmail.com>
2020-06-16 10:10:20 -0500

    hello

--- src/Question.js
+++ src/Question.js
@@ -16,19 +16,3 @@ function Question(props) {
 export default Question;
 
 
-
-// function Joke(props) {
-//     return (
-//         <div>
-//             <h3 style={{display: !props.question && "none"}}>Question: {props.question}</h3>
-//             <h3 style={{color: !props.question && "#888888"}}>Answer: {props.punchLine}</h3>
-//             <hr/>
-//         </div>
-//     )
-// }
-
-// export default Joke
-
-
-
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,6 @@ import React from "react"
 import Question from '../Question'
 import questionsData from '../questionsData'
 
-// import Joke from "../Joke"
-// import jokesData from "../jokesData"
-
-// function Home() {
-//     const jokeComponents = jokesData.map(joke => <Joke key question={joke.question} punchLine={joke.punchLine} />)
-    
-    
-//     return (
-//         <div>
-//             {jokeComponents}            
-//         </div>
-//     )
-// }
-
-// export default Home
-
-
 function Home() {
   const questionComponents = questionsData.map(question => <Question 
     question={question.question} 
@@ -39,21 +22,7 @@ function Home() {
 export default Home;
 
 
-
-
-
-// function App() {
-//     jokesData.map(joke => {
-//         return (
-//             <Joke question={joke.question} punchLine={joke.pu} />
-//         )
-//     })
-
-
-
-// <Container>
-
-    
+// <Container>  
       
 //       <Card className="text-center">
 //         <Card.Header>Question</Card.Header>
